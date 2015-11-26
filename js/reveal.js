84a5270ec6be5c913ae66ff2b672405cd10089f2
jongrover <hello@jonathangrover.com>
2015-11-25 19:00:17 -0500

    finish js version

--- js/reveal.js
+++ js/reveal.js
@@ -1,9 +1,12 @@
-// $(function(){
+$(function(){
 
-//   //listen for hover on .reveal articles
-  
-//     //fade out the img on mouse over
+  //listen for hover on .reveal articles
+  $('.reveal').hover(function () {
+    //fade out the img on mouse enter
+    $(this).find('img').fadeOut(1000);
+  }, function () {
+    //fade in the img on mouse out
+    $(this).find('img').fadeIn(1000);
+  });
 
-//     //
-
-// });
+});
